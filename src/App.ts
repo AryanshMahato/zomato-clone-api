50702c071b8a98090ed27a1a174f783bc830c377
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-09 11:51:14 +0530

    Basic routes created and switched to yarn from npm

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import cityRoutes from "./routes/cities";
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/city", cityRoutes);
 
 export default app;
